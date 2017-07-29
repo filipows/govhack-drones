f8a5450c6d08ada92c42cca67adb86dfba5c6401
Dominic May <me@mause.me>
2017-07-29 19:14:16 +0800

    Cleanup home

--- src/Home.js
+++ src/Home.js
@@ -1,64 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import BOM from './bom'
 
 class Home extends Component {
   constructor() {
     super();
-    this.bom = new BOM('rxsaxWDy3Z3fM7asszHS72HM1v1Pb3zi7jrVIGvG');
-
     this.state = {
-      val: null,
+      location: null,
       drawerOpen: false
     }
   }
 
-  determine_location() {
-    this.setState({
-      location_requested: true
-    })
-    navigator.geolocation.getCurrentPosition(loc => {
-      this.setState({loc: {
-        latitude: loc.coords.latitude,
-        longitude: loc.coords.longitude
-      }})
-    });
-  }
-
-
   render() {
     var content = null;
 
-    if (!this.state.location_requested) {
-      content = (
-        <button onClick={this.determine_location.bind(this)}>
-          Determine location
-        </button>
-      )
-
-    } else if (!this.state.loc) {
-      content = <span>Waiting for location</span>
-
-    } else if (this.state.loc && !this.state.val) {
-      this.bom
-      .forecasts()
-      .grid()
-      .interval('three-hourly')
-      .get(
-        'wind',
-        this.state.loc
-      )
-      .then(data => this.setState({val: data}));
-
-      content = <span>Loading data</span>;
+    if (this.state.location) {
+      content = <span>All good</span>
     } else {
-      content = (
-        <span>
-          {JSON.stringify(this.state.val)}
-          To get started, edit <code>src/App.js</code> and save to reload.
-          {JSON.stringify(this.state.location)}
-        </span>
-      )
+      content = <span>Waiting for location</span>
     }
 
 
